1028674c1e1f07dd03fa14a71ae07e0e78f8bb85
destinyzhang <569964924@qq.com>
2019-01-15 15:08:54 +0800

    rename

---
+++ up_shell.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#各镜像版本
+K8S_VER="v1.13.2"
+COREDNS_VER="1.2.6"
+ETCD_VER="3.2.24"
+PAUSE_VER="3.1"
+DOCKER_FILE="Dockerfile"
+
+CDIR=$(cd "$(dirname "$0")";pwd)
+echo ${CDIR}
+#遍历所有目录
+for file in $(ls -F |grep '/$'); do
+	fullpath=${CDIR}'/'${file}
+	dname=${file%?}
+	if [ ${dname:0:5} = 'kube-' ]; then
+		cd $fullpath
+		echo "FROM gcr.io/google_containers/${dname}:${K8S_VER}" >${DOCKER_FILE}
+		echo "MAINTAINER 569964924@qq.com" >>${DOCKER_FILE}
+	else if [ ${dname} = 'etcd-amd64' ];
+		cd $fullpath
+		echo "FROM gcr.io/google_containers/${dname}:${ETCD_VER}" >${DOCKER_FILE}
+		echo "MAINTAINER 569964924@qq.com" >>${DOCKER_FILE}
+	else if [ ${dname} = 'pause-amd64' ];
+		cd $fullpath
+		echo "FROM gcr.io/google_containers/${dname}:${PAUSE_VER}" >${DOCKER_FILE}
+		echo "MAINTAINER 569964924@qq.com" >>${DOCKER_FILE}
+	else if [ ${dname} = 'coredns' ];
+		cd $fullpath
+		echo "FROM gcr.io/google_containers/${dname}:${COREDNS_VER}" >${DOCKER_FILE}
+		echo "MAINTAINER 569964924@qq.com" >>${DOCKER_FILE}
+	fi
+done
