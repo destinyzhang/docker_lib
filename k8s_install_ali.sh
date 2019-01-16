2a99a497d9980e3ba00d10509444a8caabfff134
destinyzhang <569964924@qq.com>
2019-01-16 11:09:52 +0800

    k8sinstallshh

--- k8s_install_ali.sh
+++ k8s_install_ali.sh
@@ -4,11 +4,7 @@ apt-get update
 apt-get install -y apt-transport-https
 curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-
-cat <<EOF >/e/kubernetes.list
 deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
-
-
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
